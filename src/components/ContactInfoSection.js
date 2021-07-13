80ae095074934aebc7eda1350d47940a8a8fc41e
ipeglin <philip@eglin.no>
2021-07-13 21:26:58 +0200

    Created a section for displaying contact info

---
+++ src/components/ContactInfoSection.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ContactInfoSection.css';
+
+function ContactInfoSection({ contactInfo }) {
+    return (
+        <div className="wrapper">
+            <section className="contact-info">
+                <div className="contact-info-container">
+                    <div className="contact-info-box">
+                        <h2>{contactInfo.city}</h2>
+                        <p>{contactInfo.address}</p>
+                        <p>{contactInfo.postCode}</p>
+                    </div>
+                    <div className="contact-info-box">
+                        <h2>Contact info</h2>
+                        <p>{`TLF: ${contactInfo.phoneNumber}`}</p>
+                        <p>{`E-mail ${contactInfo.email}`}</p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default ContactInfoSection
