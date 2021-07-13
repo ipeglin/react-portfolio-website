8dc83f645ee68e4c29b32984cfdd34c51d094c54
ipeglin <philip@eglin.no>
2021-07-13 22:18:11 +0200

    Added a variable which contains CV information

---
+++ src/assets/CvInfo.js
@@ -0,0 +1,24 @@
+const CvInfo = {
+    highlights: [
+        {
+            title: "High School",
+            period: "2015-2018",
+            place: "Kuben videregående skole",
+            description: ""
+        },
+        {
+            title: "Hæren, Forsvaret",
+            period: "2019",
+            place: "Ingeniørbataljon",
+            description: "Logistics Staff, Supply Driver"
+        },
+        {
+            title: "University",
+            period: "2020-",
+            place: "DigSec, NTNU",
+            description: "IT education with a focus on cyber security"
+        }
+    ]
+};
+
+export default CvInfo;
