153d27e23bb1c699cff4bf44d6b7599848dea9f8
ipeglin <philip@eglin.no>
2021-07-13 12:04:56 +0200

    Changed the parameter that is passed into the herosection. The background image will instead be triggered by a dynamic hero- className

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,10 +2,9 @@ import React from 'react';
 import '../App.css';
 import './HeroSection.css';
 
-function HeroSection({header, text, image}) {
+function HeroSection({header, text, page}) {
     return (
-        <div className="hero-container">
-            <img src={image} className="hero-image"/>
+        <div className={`hero-container hero-${page}`} >
             <h2>{header}</h2>
             {text && <p>{text}</p>}
         </div>
