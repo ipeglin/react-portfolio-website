967e0ca5b134081a48bb980861ad0d9eebfbb46e
ipeglin <philip@eglin.no>
2021-07-13 11:52:45 +0200

    Created a file for the Home component

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import HeroSection from '../HeroSection';
+
+
+function Home() {
+    return (
+        <>
+            <HeroSection header="This is a test header" text="Nothing to see here"/>
+        </>
+    )
+}
+
+export default Home;
