6e464e5aa85b4b751df7a44445ecdf1b8ce50666
ipeglin <philip@eglin.no>
2021-07-13 11:04:11 +0200

    Added folder for components and wrote the functionality for a NavBar component. return is empty

---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import './NavBar.css';
+
+function NavBar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener("resize", showButton);
+
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default NavBar
