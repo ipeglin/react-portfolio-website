9c880dc94520c725f2bd415bd3a3814adc78022c
ipeglin <philip@eglin.no>
2021-07-13 11:17:19 +0200

    Wrote the return code for the navbar which uses instances of the Button component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 function NavBar() {
@@ -24,9 +25,40 @@ function NavBar() {
 
     return (
         <>
-            
+            <nav className="navbar">
+                <div classsName="navbar-container">
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                        ipeglin
+                    </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? "fas fa-times" : "fas fa-bars"} />
+                    </div>
+                    <ul className={click ? "nav-menu active" : "nav-menu"}>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/portfolio" className="nav-links" onClick={closeMobileMenu}>
+                                Portfolio
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+                                About
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/contact" className="nav-links" onClick={closeMobileMenu}>
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </>
-    )
+    );
 }
 
 export default NavBar
