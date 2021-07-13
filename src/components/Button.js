f956bfb600bced4887e4db4422d1234f8235db30
ipeglin <philip@eglin.no>
2021-07-13 11:22:34 +0200

    Added a new button style called inverted

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
 
-const STYLES = ["btn--primary", "btn--outline", "btn--test"];
+const STYLES = ["btn--primary", "btn--outline", "btn--inverted"];
 const SIZES = ["btn--medium", "btn--large"];
 
 export const Button = ({ children, type, onClick, buttonStyle, buttonSize, path }) => {
