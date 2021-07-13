7c6a6651c4af8ba787c232e142015fc3b9264871
ipeglin <philip@eglin.no>
2021-07-13 12:05:24 +0200

    Created a component for the home page content

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
+import '../../assets/img/hero-home.png'
 
 
 function Home() {
     return (
         <>
-            <HeroSection header="This is a test header" text="Nothing to see here"/>
+            <HeroSection
+                header="This is a test header"
+                text="Nothing to see here"
+                page="home" />
         </>
     )
 }
