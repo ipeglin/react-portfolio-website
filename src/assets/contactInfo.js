af7fd888952c7be2b25d678f3fec2aa3dc52c646
ipeglin <philip@eglin.no>
2021-07-13 21:26:17 +0200

    Corrected syntax when exporting. Needed to add 'export default'

---
+++ src/assets/contactInfo.js
@@ -0,0 +1,9 @@
+const contactInfo = {
+    "address": "Nina Griegs veg 16",
+    "postCode": "7046 Trondheim, NO",
+    "phoneNumber": "+47 970 85 256",
+    "email": "philip@eglin.no",
+    "city": "Trondheim"
+}
+
+export default contactInfo;
