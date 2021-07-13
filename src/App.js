6cbe7c2f90666a33062dc22d2995df3df7d53393
ipeglin <philip@eglin.no>
2021-07-13 22:19:53 +0200

    imported portfolio component and routed to it from correct URL

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
+import Portfolio from './components/pages/Portfolio';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
 
@@ -13,7 +14,7 @@ function App() {
         <NavBar />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/portfolio" exact component={Home} /> {/* CHANGE THIS*/}
+          <Route path="/portfolio" exact component={Portfolio} /> {/* CHANGE THIS*/}
           <Route path="/about" exact component={About} />
           <Route path="/contact" exact component={Contact} />
         </Switch>
