941c9d7156aa21e3a4ce457426df54168bec85ab
ipeglin <philip@eglin.no>
2021-07-13 19:18:00 +0200

    Fixed layout of footer and got SM logo link up. The foot still is NOT centered

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './Footer.css';
 import '../App.css';
 import { Link } from 'react-router-dom';
+import twLogo from '../assets/img/twitter-logo.png';
+import ghLogo from '../assets/img/gh-logo.png';
+import liLogo from '../assets/img/li-logo.png';
 
 function Footer() {
     return (
@@ -9,39 +12,45 @@ function Footer() {
             <footer>
                 <ul className="footer-links-main">
                     <li>
-                        <Link to="/" className="footer-links-pages">Home</Link>
+                        <Link to="/" className="footer-link">Home</Link>
                     </li>
                     <li>
-                        <Link to="/cases" className="footer-links-pages">Cases</Link>
+                        <Link to="/cases" className="footer-link">Cases</Link>
                     </li>
                     <li>
-                        <Link to="/portfolio" className="footer-links-pages">Portfolio</Link>
+                        <Link to="/portfolio" className="footer-link">Portfolio</Link>
                     </li>
                     <li>
-                        <Link to="/about" className="footer-links-pages">About</Link>
+                        <Link to="/about" className="footer-link">About</Link>
                     </li>
                     <li>
-                        <Link to="/contact" className="footer-links-pages">Contact</Link>
+                        <Link to="/contact" className="footer-link">Contact</Link>
                     </li>
                 </ul>
                 <ul className="footer-links-cases">
                     <li><p>Latest Cases</p></li>
                     <li>
-                        <Link to="." className="footer-links-cases">Point 1</Link>
-                        <Link to="." className="footer-links-cases">Point 2</Link>
-                        <Link to="." className="footer-links-cases">Point 3</Link>
-                        <Link to="." className="footer-links-cases">Point 4</Link>
+                        <Link to="/" className="footer-link">Point 1</Link>
+                    </li>
+                    <li>
+                        <Link to="/" className="footer-link">Point 2</Link>
+                    </li>
+                    <li>
+                        <Link to="/" className="footer-link">Point 3</Link>
+                    </li>
+                    <li>
+                        <Link to="/" className="footer-link">Point 4</Link>
                     </li>
                 </ul>
                 <div className="footer-sm">
                     <a href="https://github.com/ipeglin">
-                        <img src="../assets/img/gh-logo.png" alt="GitHub Profile" />
+                        <img src={ghLogo} alt="GitHub Profile" />
                     </a>
                     <a href="https://linkedin.com/in/ipeglin">
-                        <img src="../assets/img/li-logo.png" alt="LinkedIn Profile" />
+                        <img src={liLogo} alt="LinkedIn Profile" />
                     </a>
                     <a href="https://twitter.com/PhilipEglin">
-                        <img src="../assets/img/twitter-logo.png" alt="Twitter Profile" />
+                        <img src={twLogo} alt="Twitter Profile" />
                     </a>
                 </div>
             </footer>
