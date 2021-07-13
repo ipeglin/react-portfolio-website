6149ef5d2591e5b3090c10614567735816f656c1
ipeglin <philip@eglin.no>
2021-07-13 21:52:39 +0200

    Created the Contact Page component

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
+import contactInfo from '../../assets/contactInfo';
 import Footer from '../Footer';
+import ContactInfoSection from '../ContactInfoSection';
+import ContactFormSection from '../ContactFormSection';
 
 function Contact() {
     return (
         <>
-            <HeroSection
-                header="CONTACT PAGE"
-                text="Nothing to see here"
-                page="contact"
-            />
+            <main>
+                <HeroSection
+                    header="CONTACT PAGE"
+                    text="Nothing to see here"
+                    page="contact"
+                />
+                <ContactInfoSection contactInfo={contactInfo} />
+                <ContactFormSection />
+            </main>
             <Footer />
         </>
     )
