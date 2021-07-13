22e2178e11051468725248fed3f1e199211c5d2f
ipeglin <philip@eglin.no>
2021-07-13 23:38:11 +0200

    Added the time period to the CV highlights

--- src/components/CvSection.js
+++ src/components/CvSection.js
@@ -9,7 +9,7 @@ function CvSection({ CvInfo }) {
                 {CvInfo.map((point) => {
                     return (
                         <div className="cv-infobox">
-                            <h3 className="cv-infobox-title">{point.title}</h3>
+                            <h3 className="cv-infobox-title">{point.title} ({point.period})</h3>
                             <p className="cv-infobox-place">{point.place}</p>
                             <p className="cv-infobox-description">{point.description}</p>
                         </div>
