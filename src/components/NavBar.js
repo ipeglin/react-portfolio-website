26502b95389bcdc6855a2e08ae4534b3dd49da03
ipeglin <philip@eglin.no>
2021-07-13 14:22:55 +0200

    Redesigned the navbar to fit my wishes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -24,40 +24,17 @@ function NavBar() {
     window.addEventListener("resize", showButton);
 
     return (
-        <>
-            <nav className="navbar">
-                <div classsName="navbar-container">
-                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                        ipeglin
-                    </Link>
-                    <div className="menu-icon" onClick={handleClick}>
-                        <i className={click ? "fas fa-times" : "fas fa-bars"} />
-                    </div>
-                    <ul className={click ? "nav-menu active" : "nav-menu"}>
-                        <li className="nav-item">
-                            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/portfolio" className="nav-links" onClick={closeMobileMenu}>
-                                Portfolio
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
-                                About
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/contact" className="nav-links" onClick={closeMobileMenu}>
-                                Contact
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+        <header>
+            <Link to="/" class="navbar-brand">ipeglin</Link>
+            <nav>
+                <ul>
+                    <li><Link to="/portfolio" className="navbar-link">Portfolio</Link></li>
+                    <li><Link to="/about" className="navbar-link">About</Link></li>
+                    <li><Link to="/contact" className="navbar-link">Contact</Link></li>
+                </ul>
+                <Link to="/cases" className="navbar-cases">Cases</Link>
             </nav>
-        </>
+        </header>
     );
 }
 
