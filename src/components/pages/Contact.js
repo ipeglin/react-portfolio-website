0f94cc5781d2b11a1e4717e69d0d4451c3a6aedc
ipeglin <philip@eglin.no>
2021-07-14 15:15:42 +0200

    Corrected name of imported variables

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
-import contactInfo from '../../assets/contactInfo';
+import ContactInfo from '../../assets/ContactInfo';
 import Footer from '../Footer';
 import ContactInfoSection from '../ContactInfoSection';
 import ContactFormSection from '../ContactFormSection';
@@ -14,7 +14,7 @@ function Contact() {
                     header="STAY WITHIN REACH"
                     page="contact"
                 />
-                <ContactInfoSection contactInfo={contactInfo} />
+                <ContactInfoSection contactInfo={ContactInfo} />
                 <ContactFormSection />
             </main>
             <Footer />
