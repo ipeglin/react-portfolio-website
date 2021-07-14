fca7dfa6e78daa4d73a3dd73a4d012ac48703f12
ipeglin <philip@eglin.no>
2021-07-14 16:41:21 +0200

    Added states and effects to make sure that the component re-renders when clicking a new link in the footer

---
+++ src/components/pages/CasePage.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import _ from 'underscore';
+import Footer from '../Footer';
+import HeroSection from '../HeroSection';
+import CaseList from '../../assets/CaseList';
+import CasesInfo from '../../assets/CasesInfo';
+import { useHistory } from 'react-router';
+import Cases from './Cases';
+
+
+function CasePage(props) {
+    const history = useHistory();
+    const [page, setPage] = useState(history.location.pathname.split('/').pop());
+    useEffect(() => {
+        setPage(history.location.pathname.split('/').pop());
+    }, [history.location.pathname]);
+    
+    const [info, setInfo] = useState(_.findWhere(CasesInfo, { snowflake: page }));
+    useEffect(() => {
+        setInfo(_.findWhere(CasesInfo, { snowflake: page }));
+    }, [page]);
+
+    return (
+        <>
+            <main>
+                <HeroSection
+                    header={info.name}
+                    text={info.titleDescription}
+                    page={info.snowflake}
+                />
+            </main>
+            <Footer />
+        </>
+    )
+}
+
+export default CasePage
