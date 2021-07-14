065f82619aff446affc5516f2b5195a134df595c
ipeglin <philip@eglin.no>
2021-07-14 15:09:55 +0200

    Added Snowflake to cases

--- src/assets/casesInfo.js
+++ src/assets/casesInfo.js
@@ -1,22 +1,25 @@
-const casesInfo = [
+const CasesInfo = [
     {
         name: "Fail2Ban",
         url: "/cases/fail2ban",
+        snowFlake: "fail2ban",
         abbreviation: "f2b",
         image: "../assets/img/f2bImg.PNG"
     },
     {
         name: "Meteorologisk Institutt",
         url: "/cases/meteorologisk-institutt",
+        snowFlake: "meteorologisk-institutt",
         abbreviation: "mi",
         image: "../assets/img/miImg.PNG"
     },
     {
         name: "Incremental Backup",
         url: "/cases/incremental-backup",
+        snowFlake: "incremental-backup",
         abbreviation: "ib",
         image: "../assets/img/ibImg.PNG"
     }
 ];
 
-export default casesInfo;
+export default CasesInfo;
