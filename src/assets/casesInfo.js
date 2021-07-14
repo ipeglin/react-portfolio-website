72d2aa5f8be4656b62ff8c9cef07f1fff0728669
ipeglin <philip@eglin.no>
2021-07-14 11:40:16 +0200

    Added a file to containt cases if it should be needed later

---
+++ src/assets/casesInfo.js
@@ -0,0 +1,22 @@
+const casesInfo = [
+    {
+        name: "Fail2Ban",
+        url: "/cases/fail2ban",
+        abbreviation: "f2b",
+        image: "../assets/img/f2bImg.PNG"
+    },
+    {
+        name: "Meteorologisk Institutt",
+        url: "/cases/meteorologisk-institutt",
+        abbreviation: "mi",
+        image: "../assets/img/miImg.PNG"
+    },
+    {
+        name: "Incremental Backup",
+        url: "/cases/incremental-backup",
+        abbreviation: "ib",
+        image: "../assets/img/ibImg.PNG"
+    }
+];
+
+export default casesInfo;
