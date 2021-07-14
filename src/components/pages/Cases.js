d77fb55235f0c5a76de9313b93b93a379078a98e
ipeglin <philip@eglin.no>
2021-07-14 15:13:26 +0200

    Added a component for displaying the different cases

---
+++ src/components/pages/Cases.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../App.css';
+import CasesSection from '../CasesSection';
+import Footer from '../Footer';
+
+
+function Cases() {
+    return (
+        <>
+           <main>
+               <CasesSection />
+            </main>
+            <Footer />  
+        </>
+    )
+}
+
+export default Cases
