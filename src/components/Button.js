539c707b6c2d27d0b2368a4f829297255a3b143a
ipeglin <philip@eglin.no>
2021-07-15 00:13:53 +0200

    Rewrote the whole button component so that it is actually usefull. It will be used to link to source of project

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import './Button.css';
-import { Link } from 'react-router-dom';
 
-const STYLES = ["btn--primary", "btn--outline", "btn--inverted"];
-const SIZES = ["btn--medium", "btn--large"];
-
-export const Button = ({ children, type, onClick, buttonStyle, buttonSize, path }) => {
-    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
-    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
-
+const Button = ({ path, buttonText }) => {
     return (
-        <Link to={path} className="btn-mobile">
-            <button
-                className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-                onClick={onClick}
-                type={type}
-            >{children}</button>
-        </Link>
+        <div className="wrapper">
+            <a href={path}>
+                <div className="btn">
+                    <p>{buttonText}</p>
+                </div>
+            </a>
+        </div>
     );
 };
+
+export default Button;
