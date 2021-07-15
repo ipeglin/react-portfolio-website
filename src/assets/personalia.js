6c66a1d04990d41b5da048885138021b2ac965ed
ipeglin <philip@eglin.no>
2021-07-15 22:24:51 +0200

    Added a file that contains personalia for the about page

---
+++ src/assets/personalia.js
@@ -0,0 +1,16 @@
+const personalia = [
+    {
+        header: "About me",
+        text: `I am 21 years old, and I was born and raised in Oslo, Norway.<br><br> I have always been filled with curiousity, and found my passion for science and technology early in life.When it comes to my interests I am a jack of many trades, but being with family and friends will always be one of the things most important to me.`
+    },
+    {
+        header: "Identity",
+        text: `In my spare time I like to execise and spend my days with friends. I also like to challenge my self by broadening my knowledge in several fields; for instance programming and web development.`
+    },
+    {
+        header: "Goals",
+        text: `I love learning new things! With this in mind I wish to use my experiences to make a difference in the world, even though it might be a small one. After completing my education at NTNU Trondheim, I see myself working with electronic systems that can benefit people in the future.`
+    }
+];
+
+export default personalia;
