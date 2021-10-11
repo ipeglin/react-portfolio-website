da96751cc8d6a16a3b0baf2d04524517f7c70d7c
ipeglin <philip@eglin.no>
2021-10-11 15:41:06 +0200

    Added comments to index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+/**
+ * Rendering the App function component DOM element to root div
+ */
 ReactDOM.render(<App />, document.getElementById('root'));
 
